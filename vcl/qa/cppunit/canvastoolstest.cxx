Klomgor/core

commit 689cead9e0837dc932e3a4cd765f7d319b529018

tdf#91260 svx, sw: don't paint off-page part of drawing object
Since commit a4dee94afed9ade6ac50237c8d99a6e49d3bebc1 Writer no
longer forces drawing objects to be entirely on one page.  However since
there is only one SdrPage for the entire document, a drawing object
dangleing off the bottom of one page will be painted again on the next
page, which is clearly undesirable since Word doesn't do that
(and it also destroys the nice invariant that a fly on page N never
overlaps a fly on page N+1).

So force the SdrPageView code to ignore the drawing object on the next
page, by passing in the area of the page frame so that
ViewObjectContactOfSdrObj::isPrimitiveVisible() can verify that the
anchor position of the SdrObject is actually on the painted page.

This requires passing in another parameter; in the usual case the
DisplayInfo::maRedrawArea already contains the page frame since
commit 8af09bf33291df2fb2bfbbd6e42f9bf074fcc4fc, but there are special
cases in SwFrame::Retouch() and SwFlyFrameFormat::MakeGraphic() where
some sub-area is passed in, which cannot be used to check the anchor.

Change-Id: Ia0476216ca41dbdaa3de1063fa18cb94fe2e5ae8

sw/source/core/view/vdraw.cxx
@@ -40,6 +40,9 @@
 #include "flyfrm.hxx"
 #include <vcl/svapp.hxx>
 #include <vcl/settings.hxx>
+#include <vcl/canvastools.hxx>
+
+#include <basegfx/range/b2irectangle.hxx>
 
 #include <IDocumentDrawModelAccess.hxx>
 
@@ -84,6 +87,7 @@ void SwViewShellImp::UnlockPaint()
 
 void SwViewShellImp::PaintLayer( const SdrLayerID _nLayerID,
                             SwPrintData const*const pPrintData,
+                            SwPageFrame const& rPageFrame,
                             const SwRect& aPaintRect,
                             const Color* _pPageBackgrdColor,
                             const bool _bIsPageRightToLeft,
@@ -137,7 +141,9 @@ void SwViewShellImp::PaintLayer( const SdrLayerID _nLayerID,
             SdrView &rSdrView = const_cast< SdrView & >(GetPageView()->GetView());
             rSdrView.setHideDraw( !pPrintData->IsPrintDraw() );
         }
-        GetPageView()->DrawLayer( _nLayerID, pOutDev, pRedirector, aPaintRect.SVRect() );
+        basegfx::B2IRectangle const pageFrame(
+            vcl::unotools::b2IRectangleFromRectangle(rPageFrame.Frame().SVRect()));
+        GetPageView()->DrawLayer(_nLayerID, pOutDev, pRedirector, aPaintRect.SVRect(), &pageFrame);
         pOutDev->Pop();
 
         // reset background color of the outliner & default horiz. text dir.

include/svx/sdr/contact/displayinfo.hxx
@@ -23,6 +23,7 @@
 #include <svx/svdsob.hxx>
 #include <vcl/region.hxx>
 #include <svx/svxdllapi.h>
+#include <basegfx/range/b2irectangle.hxx>
 
 namespace sdr
 {
@@ -37,6 +38,8 @@ namespace sdr
             // is empty, everything needs to be redrawn
             vcl::Region                                          maRedrawArea;
 
+            /// only for Writer: current page being painted
+            basegfx::B2IRectangle m_WriterPageFrame;
 
             // Internal flag to know when the control layer is painted. Default is
             // false. If set to true, painting of the page, page borders and
@@ -77,6 +80,9 @@ namespace sdr
             // access to RedrawArea
             void SetRedrawArea(const vcl::Region& rRegion);
             const vcl::Region& GetRedrawArea() const { return maRedrawArea; }
+
+            void SetWriterPageFrame(basegfx::B2IRectangle const& rPageFrame);
+            basegfx::B2IRectangle const& GetWriterPageFrame() const { return m_WriterPageFrame; }
 
             // Access to ControlLayerProcessingActive flag
             void SetControlLayerProcessingActive(bool bDoPaint);

include/svx/sdrpagewindow.hxx
@@ -39,7 +39,7 @@ namespace sdr
     }
 }
 
-namespace basegfx { class B2DRange; }
+namespace basegfx { class B2DRange; class B2IRange; }
 
 class SdrPaintWindow;
 class SdrPageView;
@@ -74,7 +74,7 @@ public:
     void PrePaint();
     void PrepareRedraw(const vcl::Region& rReg);
     void RedrawAll( sdr::contact::ViewObjectContactRedirector* pRedirector );
-    void RedrawLayer( const SdrLayerID* pId, sdr::contact::ViewObjectContactRedirector* pRedirector );
+    void RedrawLayer( const SdrLayerID* pId, sdr::contact::ViewObjectContactRedirector* pRedirector, basegfx::B2IRange const*);
 
     // Invalidate call, used from ObjectContact(OfPageView) in InvalidatePartOfView(...)
     void InvalidatePageWindow(const basegfx::B2DRange& rRange);

include/svx/svdpagv.hxx
@@ -162,7 +162,8 @@ public:
     void setPreparedPageWindow(SdrPageWindow* pKnownTarget);
 
     void DrawLayer(SdrLayerID nID, OutputDevice* pGivenTarget, sdr::contact::ViewObjectContactRedirector* pRedirector = nullptr,
-                   const Rectangle& rRect =  Rectangle());
+                   const Rectangle& rRect = Rectangle(),
+                   basegfx::B2IRange const* pPageFrame = nullptr);
     void DrawPageViewGrid(OutputDevice& rOut, const Rectangle& rRect, Color aColor = Color( COL_BLACK ) );
 
     Rectangle GetPageRect() const;

svx/source/sdr/contact/displayinfo.cxx
@@ -50,6 +50,11 @@ namespace sdr
             maRedrawArea = rRegion;
         }
 
+        void DisplayInfo::SetWriterPageFrame(basegfx::B2IRectangle const& rPageFrame)
+        {
+            m_WriterPageFrame = rPageFrame;
+        }
+
         void DisplayInfo::SetControlLayerProcessingActive(bool bDoProcess)
         {
             if((bool)mbControlLayerProcessingActive != bDoProcess)

svx/source/sdr/contact/viewobjectcontactofsdrobj.cxx
@@ -30,6 +30,7 @@
 #include <svx/svdoole2.hxx>
 #include <svx/svdview.hxx>
 #include <vcl/outdev.hxx>
+#include <vcl/canvastools.hxx>
 
 #include "fmobj.hxx"
 
@@ -136,6 +137,22 @@ bool ViewObjectContactOfSdrObj::isPrimitiveVisible(const DisplayInfo& rDisplayIn
                     return false;
                 }
             }
+        }
+    }
+
+    // tdf#91260 check if the object is anchored on a different Writer page
+    // than the one being painted, and if so ignore it (Writer has only one
+    // SdrPage, so the part of the object that should be visible will be
+    // painted on the page where it is anchored)
+    // Note that we cannot check the ViewInformation2D ViewPort for this
+    // because it is only the part of the page that is currently visible.
+    basegfx::B2IPoint const& rAnchor(vcl::unotools::b2IPointFromPoint(getSdrObject().GetAnchorPos()));
+    if (rAnchor.getX() || rAnchor.getY()) // only Writer sets anchor position
+    {
+        if (!rDisplayInfo.GetWriterPageFrame().isEmpty() &&
+            !rDisplayInfo.GetWriterPageFrame().isInside(rAnchor))
+        {
+            return false;
         }
     }
 

svx/source/svdraw/sdrpagewindow.cxx
@@ -352,7 +352,9 @@ void SdrPageWindow::RedrawAll( sdr::contact::ViewObjectContactRedirector* pRedir
 #endif // CLIPPER_TEST
 }
 
-void SdrPageWindow::RedrawLayer( const SdrLayerID* pId, sdr::contact::ViewObjectContactRedirector* pRedirector )
+void SdrPageWindow::RedrawLayer(const SdrLayerID* pId,
+        sdr::contact::ViewObjectContactRedirector* pRedirector,
+        basegfx::B2IRectangle const*const pPageFrame)
 {
     // set redirector
     GetObjectContact().SetViewObjectContactRedirector(pRedirector);
@@ -394,6 +396,11 @@ void SdrPageWindow::RedrawLayer( const SdrLayerID* pId, sdr::contact::ViewObject
         // Writer or calc, coming from original RedrawOneLayer.
         // #i72889# no page painting for layer painting
         aDisplayInfo.SetPageProcessingActive(false);
+
+        if (pPageFrame) // Writer page frame for anchor based clipping
+        {
+            aDisplayInfo.SetWriterPageFrame(*pPageFrame);
+        }
 
         // paint page
         GetObjectContact().ProcessDisplay(aDisplayInfo);

svx/source/svdraw/svdpagv.cxx
@@ -45,6 +45,7 @@
 #include <svx/sdrpagewindow.hxx>
 #include <svx/sdrpaintwindow.hxx>
 #include <comphelper/lok.hxx>
+#include <basegfx/range/b2irectangle.hxx>
 
 using namespace ::com::sun::star;
 
@@ -305,7 +306,9 @@ void SdrPageView::setPreparedPageWindow(SdrPageWindow* pKnownTarget)
     mpPreparedPageWindow = pKnownTarget;
 }
 
-void SdrPageView::DrawLayer( SdrLayerID nID, OutputDevice* pGivenTarget, sdr::contact::ViewObjectContactRedirector* pRedirector, const Rectangle& rRect )
+void SdrPageView::DrawLayer(SdrLayerID nID, OutputDevice* pGivenTarget,
+        sdr::contact::ViewObjectContactRedirector* pRedirector,
+        const Rectangle& rRect, basegfx::B2IRectangle const*const pPageFrame)
 {
     if(GetPage())
     {
@@ -316,7 +319,7 @@ void SdrPageView::DrawLayer( SdrLayerID nID, OutputDevice* pGivenTarget, sdr::co
             if(pKnownTarget)
             {
                 // paint known target
-                pKnownTarget->RedrawLayer(&nID, pRedirector);
+                pKnownTarget->RedrawLayer(&nID, pRedirector, nullptr);
             }
             else
             {
@@ -346,12 +349,11 @@ void SdrPageView::DrawLayer( SdrLayerID nID, OutputDevice* pGivenTarget, sdr::co
                         aTemporaryPaintWindow.SetRedrawRegion(rExistingRegion);
                     else
                         aTemporaryPaintWindow.SetRedrawRegion(vcl::Region(rRect));
-
                     // patch the ExistingPageWindow
                     pPreparedTarget->patchPaintWindow(aTemporaryPaintWindow);
 
                     // redraw the layer
-                    pPreparedTarget->RedrawLayer(&nID, pRedirector);
+                    pPreparedTarget->RedrawLayer(&nID, pRedirector, pPageFrame);
 
                     // restore the ExistingPageWindow
                     pPreparedTarget->unpatchPaintWindow();
@@ -377,7 +379,7 @@ void SdrPageView::DrawLayer( SdrLayerID nID, OutputDevice* pGivenTarget, sdr::co
                         aTemporaryPaintWindow.SetRedrawRegion(rExistingRegion);
                     }
 
-                    aTemporaryPageWindow.RedrawLayer(&nID, pRedirector);
+                    aTemporaryPageWindow.RedrawLayer(&nID, pRedirector, nullptr);
                 }
             }
         }
@@ -387,7 +389,7 @@ void SdrPageView::DrawLayer( SdrLayerID nID, OutputDevice* pGivenTarget, sdr::co
             for(sal_uInt32 a(0L); a < PageWindowCount(); a++)
             {
                 SdrPageWindow* pTarget = GetPageWindow(a);
-                pTarget->RedrawLayer(&nID, pRedirector);
+                pTarget->RedrawLayer(&nID, pRedirector, nullptr);
             }
         }
     }

commit 7f1a2f8fa2ce9ddf15f0e32b9faa01d84b60a8bb

tdf#124987 sw: Fixed off-by-one error in draw obj anchor clipping rect
Since tdf#91260, Writer clips drawing objects to pages by checking
whether their anchors are contained by a bounding rectangle. The code
that calculated this rectangle had an off-by-one error causing the
clipping rectangle to always be exactly 1 twip shorter and narrower than
the actual page frame rectangle. As a result, anything with an anchor
position on the right or bottom edge of the screen would fail to render
- including any objects anchored to an RTL page.

Regression from commit 689cead9e0837dc932e3a4cd765f7d319b529018
  Author: Michael Stahl <mstahl@redhat.com>
  Date:   Tue Dec 6 22:59:40 2016 +0100
  tdf#91260 svx, sw: don't paint off-page part of drawing object

Change-Id: Ife6c817f0689ca2b31d22df31f740c30ab548c8c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/205114
Reviewed-by: Jonathan Clark <jonathan@libreoffice.org>
Tested-by: Jenkins

include/vcl/canvastools.hxx
@@ -161,6 +161,7 @@ namespace vcl::unotools
         tools::Rectangle                   VCL_DLLPUBLIC rectangleFromB2IRectangle( const basegfx::B2IRange& );
         basegfx::B2IPoint           VCL_DLLPUBLIC b2IPointFromPoint(const Point&);
         basegfx::B2IRectangle       VCL_DLLPUBLIC b2IRectangleFromRectangle(const tools::Rectangle&);
+        basegfx::B2IRectangle       VCL_DLLPUBLIC b2IRectangleFromClosedRectangle(const tools::Rectangle&);
 
 }
 

sw/source/core/view/vdraw.cxx
@@ -136,7 +136,11 @@ void SwViewShellImp::PaintLayer( const SdrLayerID _nLayerID,
     }
 
     pOutDev->Push( vcl::PushFlags::LINECOLOR );
-    basegfx::B2IRectangle const pageFrame = vcl::unotools::b2IRectangleFromRectangle(rPageFrame.getFrameArea().SVRect());
+
+    // tdf#124987: Writer considers frame rectangles closed ranges, and may position anchors
+    // at the edges of the rectangle. Convert this to B2IRectangle as a closed range, not open.
+    basegfx::B2IRectangle const pageFrame
+        = vcl::unotools::b2IRectangleFromClosedRectangle(rPageFrame.getFrameArea().SVRect());
     GetPageView()->DrawLayer(_nLayerID, pOutDev, pRedirector, aPaintRect.SVRect(), &pageFrame);
     pOutDev->Pop();
 

vcl/CppunitTest_vcl_text.mk
@@ -16,6 +16,7 @@ $(eval $(call gb_CppunitTest_set_include,vcl_text,\
 
 $(eval $(call gb_CppunitTest_add_exception_objects,vcl_text, \
 	vcl/qa/cppunit/canvasbitmaptest \
+	vcl/qa/cppunit/canvastoolstest \
 	vcl/qa/cppunit/complextext \
 	vcl/qa/cppunit/justificationdata \
 	vcl/qa/cppunit/text \

vcl/source/helper/canvastools.cxx
@@ -338,6 +338,19 @@ namespace vcl::unotools
                                   rRect.IsHeightEmpty() ? rRect.Top() : rRect.Bottom() );
         }
 
+        basegfx::B2IRectangle b2IRectangleFromClosedRectangle(tools::Rectangle const& rRect)
+        {
+            // although B2IRange internally has separate height/width emptiness, it doesn't
+            // expose any API to let us set them separately, so just do the best we can.
+            if (rRect.IsWidthEmpty() && rRect.IsHeightEmpty())
+            {
+                return basegfx::B2IRange(basegfx::B2ITuple(rRect.Left(), rRect.Top()));
+            }
+
+            return basegfx::B2IRange(rRect.Left(), rRect.Top(), rRect.Left() + rRect.GetWidth(),
+                                     rRect.Top() + rRect.GetHeight());
+        }
+
         basegfx::B2DSize b2DSizeFromSize(const Size& rSize)
         {
             return basegfx::B2DSize(rSize.Width(), rSize.Height());

vcl/qa/cppunit/canvastoolstest.cxx
@@ -1,0 +1,60 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+
+#include <test/bootstrapfixture.hxx>
+#include <vcl/canvastools.hxx>
+
+using namespace ::com::sun::star;
+using namespace vcl::unotools;
+
+namespace
+{
+class CanvasToolsTest : public CppUnit::TestFixture
+{
+public:
+    void testB2IRectangleFromClosedRectangle();
+
+    CPPUNIT_TEST_SUITE(CanvasToolsTest);
+    CPPUNIT_TEST(testB2IRectangleFromClosedRectangle);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+void CanvasToolsTest::testB2IRectangleFromClosedRectangle()
+{
+    // tdf#124987: Characterization test for a use that was causing an off-by-one
+    // error when clipping draw objects to the page containing the anchor
+
+    tools::Rectangle aRect{ /*nLeft*/ 100, /*nTop*/ 100, /*nRight*/ 200, /*nBottom*/ 300 };
+
+    // With the original code, the simulated RTL anchor would be considered off-screen
+    auto aBadPageFrame = b2IRectangleFromRectangle(aRect);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(200), aBadPageFrame.getMaxX());
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(300), aBadPageFrame.getMaxY());
+
+    // With the new code, the simulated RTL anchor would be considered on-screen
+    auto aPageFrame = b2IRectangleFromClosedRectangle(aRect);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(201), aPageFrame.getMaxX());
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(301), aPageFrame.getMaxY());
+}
+
+} // namespace
+
+CPPUNIT_TEST_SUITE_REGISTRATION(CanvasToolsTest);
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */