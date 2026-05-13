4d1e1c3f4c5635af86f8a337e41f0a8fb2bfbf47
Michael Weghorn <m.weghorn@posteo.de>
2026-05-13 12:38:36 +0200

    tdf#130857 qt weld: Support Calc "Character" dialog
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Calc
    * double-click into cell A1 to enter edit mode
    * type "eee"
    * select the text
    * right-click on the text to open the context menu
    * activate the "Character..." context menu entry
    
    Change-Id: I7905d0105e133b34b0ddde9c19848fbab86e425f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/205164
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -156,6 +156,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/BasicIDE/ui/newlibdialog.ui",
     u"modules/sbibliography/ui/choosedatasourcedialog.ui",
     u"modules/sbibliography/ui/mappingdialog.ui",
+    u"modules/scalc/ui/chardialog.ui",
     u"modules/scalc/ui/colwidthdialog.ui",
     u"modules/scalc/ui/condformatmanager.ui",
     u"modules/scalc/ui/conditionalformatdialog.ui",
